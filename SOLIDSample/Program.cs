da2950efec25aa6e94d8ab4f20aed48a8eb4f68c
Adriano Cavalari <adriano@prinks.com>
2019-07-21 22:50:08 +0100

    Add project files.

---
+++ SOLIDSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SOLIDSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
